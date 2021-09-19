65f8322d21e7f378d708fb9bbc957cbdabeab035
Muhammad-Bilal-7896 <bilalmohib7896@gmail.com>
2021-09-20 03:10:27 +0500

    Signed-off-by: Muhammad-Bilal-7896 <bilalmohib7896@gmail.com>

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
+app.use
 
 const port = process.env.PORT || 8080;
 app.listen(port,()=>console.log(`Listening on port ${port}... `));
