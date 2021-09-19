2796cf7dd9d1498ea77715d4cfcf45cab67ab845
Muhammad-Bilal-7896 <bilalmohib7896@gmail.com>
2021-09-20 03:03:23 +0500

    Writing the code for server

---
+++ server/index.js
@@ -0,0 +1,8 @@
+const cors = require("cors");
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const port = process.env.PORT || 8080;
+app.listen(port,()=>console.log(`Listening on port ${port}... `));
